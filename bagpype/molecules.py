b0fd24bc9ff52f8486c8dcecba945858cc6f8e6f
FlorianSong <florian.song13@imperial.ac.uk>
2020-05-02 17:49:11 +0100

    Reverted previous commit

--- bagpype/molecules.py
+++ bagpype/molecules.py
@@ -301,7 +301,7 @@ class Bond(object):
         self.id = id
         self.atom1 = atom1
         self.atom2 = atom2
-        self.weight = round(weight, 16)
+        self.weight = weight
         if isinstance(types, list):
             self.bond_type = types
         else:
