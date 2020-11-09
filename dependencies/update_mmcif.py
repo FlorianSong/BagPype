dff54115a6348b1adca4c7ae152db9ef2802a648
Proteinlens Admin <proteinlens_admin@ch-bjorn.ch.ic.ac.uk>
2020-11-09 15:47:41 +0000

    Fixed issue with update_mmcif script

--- dependencies/update_mmcif.py
+++ dependencies/update_mmcif.py
@@ -15,8 +15,9 @@ with open(file_name) as f:
     for line in f:
         if line.startswith("data"):
             current_file_connection.close()
-            current_code = line[-4:].strip()
-            print(current_code)
+            current_code = line[5:].strip()
+            if len(current_code) == 1: print(current_code)
+            # print(current_code)
             current_file = "mmcif/" + current_code + ".cif"
             current_file_connection = open(current_file, "w")
             current_file_connection.write(line)
