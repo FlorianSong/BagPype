5624436d4d32c89eaff932a6f4a864a221ceac63
Florian Song <FlorianSong@users.noreply.github.com>
2021-04-30 17:51:00 +0100

    Corrected small mistake in pi-pi stacking

--- bagpype/construction.py
+++ bagpype/construction.py
@@ -1818,8 +1818,7 @@ class Graph_constructor(object):
                         for n in range(3):
                             electro += (
                                 # (-self.k_factor * 4.184 / 6.022)
-                                -1
-                                * (332 / epsilon)
+                                (332 / epsilon)
                                 * q1[m]
                                 * q2[n]
                                 / (np.linalg.norm(c1[m] - c2[n]))
