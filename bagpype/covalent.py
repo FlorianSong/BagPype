01c1e493ecfee96ec02fda9f1215d9e4d54cff45
FlorianSong <florian.song13@imperial.ac.uk>
2021-05-10 17:00:21 +0100

    Same for covalent

--- bagpype/covalent.py
+++ bagpype/covalent.py
@@ -1,28 +1,35 @@
-# Script to generate the amino acid covalent bond energies dictionary in "energies.py".
-# Florian Song, Feb 2017
-# The needed library can be downloaded at:
-# ftp://ftp.ebi.ac.uk/pub/databases/msd/pdbechem/files/mmcif.tar.gz
-# After the download, unpack the tar and deposit in the same dir as this script.
-# References for bond energies: 
-# Huheey - Inorganic Chemistry 
-# Yu-Ran Luo - Comprehensive handbook of chemical bond energies
+"""
+Script to generate a dictionary of amino acid covalent bond energies.
+This is needed to speed up the construction of covalent bonds massively.
+Florian Song, Feb 2017
+The needed library can be downloaded at:
+ftp://ftp.ebi.ac.uk/pub/databases/msd/pdbechem/files/mmcif.tar.gz
+(After the download, unpack the tar and deposit in the same dir as this script.)
+There's a script in dependencies, that does this automatically!
+
+References for bond energies: 
+Huheey - Inorganic Chemistry 
+Yu-Ran Luo - Comprehensive handbook of chemical bond energies
+"""
 
 import os
 import bagpype.parameters
 from bagpype.errors import MissingEnergyError, UnknownResidueError, UnusualCIFFile
 from CifFile import ReadCif
+
 # Bond energies in kJ/mol taken from parameters.py
 
 
-def generate_energies_dictionary(AA, nomenclature = "new"):
+def generate_energies_dictionary(AA, nomenclature="new"):
     """
-    Input: 
-    - LIST of amino acids or any residue identifier 
+    The main function to generate a dictionary of COVALENT bond energies
+    Input:
+    - LIST of amino acids or any residue identifier
     - nomenclature: either "new" (True) or "old" (False)
     Output: Dictionary of bond energies as required by parsing.py
     """
     final_all_residues_output_dict = {}
-    
+
     AA = [AA] if isinstance(AA, str) else AA
 
     print(("Initialising covalent bond energies for " + ", ".join(AA)))
@@ -30,7 +37,13 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
     for aa in AA:
 
         try:
-            file = open(os.path.dirname(os.path.dirname(bagpype.__file__)) + "/dependencies/mmcif/" + aa + '.cif', "r")
+            file = open(
+                os.path.dirname(os.path.dirname(bagpype.__file__))
+                + "/dependencies/mmcif/"
+                + aa
+                + ".cif",
+                "r",
+            )
         except IOError:
             raise UnknownResidueError(str(aa))
 
@@ -40,26 +53,24 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
         parsed_cif = ReadCif(file)
         lower_case_aa = aa.lower()
 
-
         # Extract covalent bond information from the dictionary
         try:
-            atom_list = parsed_cif[lower_case_aa]['_chem_comp_atom.atom_id']
-            element_list = parsed_cif[lower_case_aa]['_chem_comp_atom.type_symbol']
+            atom_list = parsed_cif[lower_case_aa]["_chem_comp_atom.atom_id"]
+            element_list = parsed_cif[lower_case_aa]["_chem_comp_atom.type_symbol"]
             aromaticity = parsed_cif[lower_case_aa]["_chem_comp_atom.pdbx_aromatic_flag"]
         except KeyError:
             raise UnusualCIFFile(aa)
 
         try:
-            bond_list1 = parsed_cif[lower_case_aa]['_chem_comp_bond.atom_id_1']
-            bond_list2 = parsed_cif[lower_case_aa]['_chem_comp_bond.atom_id_2']
-            bond_types = parsed_cif[lower_case_aa]['_chem_comp_bond.value_order']
+            bond_list1 = parsed_cif[lower_case_aa]["_chem_comp_bond.atom_id_1"]
+            bond_list2 = parsed_cif[lower_case_aa]["_chem_comp_bond.atom_id_2"]
+            bond_types = parsed_cif[lower_case_aa]["_chem_comp_bond.value_order"]
             bond_list1 = [bond_list1] if isinstance(bond_list1, str) else bond_list1
             bond_list2 = [bond_list2] if isinstance(bond_list2, str) else bond_list2
             bond_types = [bond_types] if isinstance(bond_types, str) else bond_types
         except KeyError:
             print("    Just to check: ", aa, " is a single atom without bonds.")
             continue
-
 
         #####################################################
         # Building of the bond and bond-strength dictionary #
@@ -68,7 +79,6 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
         # Initialisation
         final_dict = {}
         bond_list_for_cycle_detection = []
-
 
         # Initialise all keys, ie every atom
         for atom in atom_list:
@@ -85,8 +95,12 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
             atom1 = bond_list1[i]
             atom2 = bond_list2[i]
             bond_type = bond_types[i]
-            atom1_element = parsed_cif[lower_case_aa]['_chem_comp_atom.type_symbol'][parsed_cif[lower_case_aa]['_chem_comp_atom.atom_id'].index(atom1)]
-            atom2_element = parsed_cif[lower_case_aa]['_chem_comp_atom.type_symbol'][parsed_cif[lower_case_aa]['_chem_comp_atom.atom_id'].index(atom2)]
+            atom1_element = parsed_cif[lower_case_aa]["_chem_comp_atom.type_symbol"][
+                parsed_cif[lower_case_aa]["_chem_comp_atom.atom_id"].index(atom1)
+            ]
+            atom2_element = parsed_cif[lower_case_aa]["_chem_comp_atom.type_symbol"][
+                parsed_cif[lower_case_aa]["_chem_comp_atom.atom_id"].index(atom2)
+            ]
 
             # This is a tuple for looking up the bond strength from the dictionaries defined above
             # Obviously the reverse is also needed, since the dictionaries above are only one way
@@ -99,7 +113,9 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
                     bond_strength = bagpype.parameters.single_bond_energies[lookup]
                 except KeyError:
                     try:
-                        bond_strength = bagpype.parameters.single_bond_energies[lookup_reversed]
+                        bond_strength = bagpype.parameters.single_bond_energies[
+                            lookup_reversed
+                        ]
                     except KeyError:
                         raise MissingEnergyError(atom1_element, atom2_element, aa, bond_type)
             elif bond_type == "DOUB":
@@ -107,7 +123,9 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
                     bond_strength = bagpype.parameters.double_bond_energies[lookup]
                 except KeyError:
                     try:
-                        bond_strength = bagpype.parameters.double_bond_energies[lookup_reversed]
+                        bond_strength = bagpype.parameters.double_bond_energies[
+                            lookup_reversed
+                        ]
                     except KeyError:
                         raise MissingEnergyError(atom1_element, atom2_element, aa, bond_type)
             elif bond_type == "TRIP":
@@ -115,20 +133,20 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
                     bond_strength = bagpype.parameters.triple_bond_energies[lookup]
                 except KeyError:
                     try:
-                        bond_strength = bagpype.parameters.triple_bond_energies[lookup_reversed]
+                        bond_strength = bagpype.parameters.triple_bond_energies[
+                            lookup_reversed
+                        ]
                     except KeyError:
                         raise MissingEnergyError(atom1_element, atom2_element, aa, bond_type)
             else:
                 raise UnusualCIFFile(aa)
 
-
             # Add bond to the dictionary both ways
             final_dict[atom1][atom2] = final_dict[atom2][atom1] = bond_strength
 
-
             # Build a list of bonds for cycle (aromatic ring) detection later on
             # In order to limit computational expense, bonds with Hs are excluded, as they will never be part of aromatic rings
-            if atom1_element != 'H' and atom2_element != 'H':
+            if atom1_element != "H" and atom2_element != "H":
                 bond_list_for_cycle_detection.append([atom1, atom2])
 
         # Usually, DNA endpoints have a H atom instead of another phosphate link. This takes that into account:
@@ -137,13 +155,11 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
             # final_dict["O5'"] = {}
             final_dict["O5'"]["HO5'"] = final_dict["HO5'"]["O5'"] = 459
 
-
-
         ##################
         # Bond averaging #
         ##################
         def average_bond(atom, neighbours, bonds_dictionary):
-            """ Input: 
+            """Input:
             atom = single atom_name
             neighbours = 2-element list of neighbours
             bonds_dictionary = dictionary where the bonds are to be changed
@@ -153,7 +169,7 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
                 raise Exception("Bond averaging received more than two atoms! ")
             original_bond_strength1 = bonds_dictionary[atom][neighbours[0]]
             original_bond_strength2 = bonds_dictionary[atom][neighbours[1]]
-            new_bond_strength = (original_bond_strength1 + original_bond_strength2) / 2.
+            new_bond_strength = (original_bond_strength1 + original_bond_strength2) / 2.0
             bonds_dictionary[atom][neighbours[0]] = new_bond_strength
             bonds_dictionary[atom][neighbours[1]] = new_bond_strength
             bonds_dictionary[neighbours[0]][atom] = new_bond_strength
@@ -161,13 +177,12 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
 
             return bonds_dictionary
 
-
         ###########################
-        # Aromatic Ring Detection # 
+        # Aromatic Ring Detection #
         ###########################
 
         def find_cycles(list_of_nodes):
-            """ Function for finding cycles in undirected graphs 
+            """Function for finding cycles in undirected graphs
             Taken from networkx's cycle_basis()
             Why not use networkx one may ask? This way, the output can be better manipulated and the source code is right here for reference
             Input: A list of lists, each sublist containing two atom names.
@@ -178,86 +193,119 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
             while unique:
                 root = unique.pop()
                 stack = [root]
-                pred = {root:root}
-                used = {root:set()}
+                pred = {root: root}
+                used = {root: set()}
                 while stack:
                     z = stack.pop()
                     zused = used[z]
 
-                    neighbours = [item for sub in [[j for j in i if j is not z] for i in list_of_nodes if z in i] for item in sub]
+                    neighbours = [
+                        item
+                        for sub in [
+                            [j for j in i if j is not z] for i in list_of_nodes if z in i
+                        ]
+                        for item in sub
+                    ]
                     for nbr in neighbours:
-                        if nbr not in used:   # new node
-                            pred[nbr]=z
+                        if nbr not in used:  # new node
+                            pred[nbr] = z
                             stack.append(nbr)
-                            used[nbr]=set([z])
-                        elif nbr == z:        # self loops
+                            used[nbr] = set([z])
+                        elif nbr == z:  # self loops
                             cycles.append([z])
-                        elif nbr not in zused:# found a cycle
-                            pn=used[nbr]
-                            cycle=[nbr,z]
-                            p=pred[z]
+                        elif nbr not in zused:  # found a cycle
+                            pn = used[nbr]
+                            cycle = [nbr, z]
+                            p = pred[z]
                             while p not in pn:
                                 cycle.append(p)
-                                p=pred[p]
+                                p = pred[p]
                             cycle.append(p)
                             cycles.append(cycle)
                             used[nbr].add(z)
-                unique-=set(pred)
-                root=None
+                unique -= set(pred)
+                root = None
             return [i for i in cycles if len(i) > 1]
             # return [ item for sub in [i for i in cycles if len(i) > 1] for item in sub]
 
         # Run above function and find aromatic rings
         aromatic_rings = find_cycles(bond_list_for_cycle_detection)
-        
+
         # check whether it's 6*C
-        accepted_aromatic_rings = [ring for ring in aromatic_rings 
-                                    if len(ring) == 6 and 
-                                        all([ (element_list[ring_member_index] == 'C' and aromaticity[ring_member_index] == "Y") 
-                                            for ring_member_index in [atom_list.index(ring_member) for ring_member in ring]])
+        accepted_aromatic_rings = [
+            ring
+            for ring in aromatic_rings
+            if len(ring) == 6
+            and all(
+                [
+                    (
+                        element_list[ring_member_index] == "C"
+                        and aromaticity[ring_member_index] == "Y"
+                    )
+                    for ring_member_index in [
+                        atom_list.index(ring_member) for ring_member in ring
+                    ]
+                ]
+            )
         ]
 
         # Make this somewhat deterministic by sorting
         accepted_aromatic_rings.sort()
-        
+
         # Change corresponding entries in the dictionary
         for ring in accepted_aromatic_rings:
             for i, atom in enumerate(ring):
-                if i%2 == 0:
+                if i % 2 == 0:
                     neighbours = [i for i in list(final_dict[atom].keys()) if i in ring]
                     # neighbours
                     final_dict = average_bond(atom, neighbours, final_dict)
-
 
         ################################################
         # Carboxylic acid and Guanidin group detection #
         ################################################
 
         # Please put in all the amino acids that should have averaging HERE:
-        amino_acids_for_nonring_bond_averaging = ['ARG', 'ASP', 'GLU', 'KCX']
+        amino_acids_for_nonring_bond_averaging = ["ARG", "ASP", "GLU", "KCX"]
 
         # Detection of the specific groups that should be averaged
-        if parsed_cif[lower_case_aa]['_chem_comp.id'] in amino_acids_for_nonring_bond_averaging:
+        if (
+            parsed_cif[lower_case_aa]["_chem_comp.id"]
+            in amino_acids_for_nonring_bond_averaging
+        ):
 
             # Find all nontrivial Carbons (ie not "C") which have not been flagged as aromatic in the cif file
-            nontrivial_C = [i for i in list(final_dict.keys()) if i != 'C' and i[0] == 'C' and parsed_cif[lower_case_aa]["_chem_comp_atom.pdbx_aromatic_flag"][atom_list.index(i)] == 'N']
+            nontrivial_C = [
+                i
+                for i in list(final_dict.keys())
+                if i != "C"
+                and i[0] == "C"
+                and parsed_cif[lower_case_aa]["_chem_comp_atom.pdbx_aromatic_flag"][
+                    atom_list.index(i)
+                ]
+                == "N"
+            ]
 
             for carbon in nontrivial_C:
-                neighbours2 = [i for i in list(final_dict[carbon].keys()) if i[0] not in ['C', 'H']] 
+                neighbours2 = [
+                    i for i in list(final_dict[carbon].keys()) if i[0] not in ["C", "H"]
+                ]
                 if len(neighbours2) > 1:
 
                     # Select the two nonC atoms with the same first two letters, not sure whether this is always the case, but works for the three amino acids
-                    neighbours2 = [i for i in neighbours2 for j in [x for x in neighbours2 if x != i] if i[0:2] == j[0:2]] # This is wayyy too long...
+                    neighbours2 = [
+                        i
+                        for i in neighbours2
+                        for j in [x for x in neighbours2 if x != i]
+                        if i[0:2] == j[0:2]
+                    ]  # This is wayyy too long...
 
                     # Finally, average the bonds:
                     final_dict = average_bond(carbon, neighbours2, final_dict)
-
 
         ############################
         # Include "old" atom_names #
         ############################
         # Note that old here means the "old" nomenclature not yet included in the above dictionary and "new" is the one with which the above was generated
-
 
         # old_atom_names = parsed_cif[lower_case_aa]['_chem_comp_atom.alt_atom_id']
 
@@ -284,13 +332,13 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
         #     for nb in neighbours3: # new nomenclature
         #         if nb in difference_new_list:
 
-        #             which_position = difference_new_list.index(nb) 
+        #             which_position = difference_new_list.index(nb)
         #             old_name = difference_old_list[which_position] # old nomenclature
-                    
+
         #             # add same bond strength
         #             dict_to_add[old_name] = previous_dict[nb]
 
-        #         else: 
+        #         else:
 
         #             # Don't forget to change the corresponding entries going the other way
         #             final_dict[nb][old_atom] = final_dict[nb][new_atom]
@@ -303,7 +351,9 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
 
         # Put the resulting dictionary into a dictionary with the id at the front and then add that to the outputs
         to_add_to_final_all_residues_output_dict = {}
-        to_add_to_final_all_residues_output_dict[parsed_cif[lower_case_aa]['_chem_comp.id']] = final_dict
+        to_add_to_final_all_residues_output_dict[
+            parsed_cif[lower_case_aa]["_chem_comp.id"]
+        ] = final_dict
         final_all_residues_output_dict.update(to_add_to_final_all_residues_output_dict)
 
     ###################
@@ -311,4 +361,3 @@ def generate_energies_dictionary(AA, nomenclature = "new"):
     ###################
 
     return final_all_residues_output_dict
-
