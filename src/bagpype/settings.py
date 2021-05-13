f3ac528d5959f1444ae5ff34cfa45ac5e85bc1de
FlorianSong <florian.song13@imperial.ac.uk>
2021-05-13 20:20:18 +0100

    First step of packaging the project, create src folder

--- src/bagpype/settings.py
+++ src/bagpype/settings.py
@@ -1,7 +1,8 @@
 import bagpype
 import os
+
 DEPENDENCIES_ROOT = os.path.dirname(os.path.dirname(bagpype.__file__)) + "/dependencies/"
 
-# Capping parameter - number of decimals to cap to 
+# Capping parameter - number of decimals to cap to
 # This is to improve numeric stability across OS's and computers
 capping_decimals = 10
---
+++ src/bagpype/setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name="bagpype",
+    version="",
+    description="",
+    author="Ben Amor, Francesca Vianello, Florian Song",
+    author_email="",
+    url="",
+    packages=["numpy", "scipy", "networkx", "pandas", "matplotlib", "PyCifRw"],
+)
