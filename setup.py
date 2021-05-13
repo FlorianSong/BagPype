864a243e353dc6cf926e4792cc34a890160ff255
FlorianSong <florian.song13@imperial.ac.uk>
2021-05-13 22:05:37 +0100

    Final stage of packaging, should be good now

--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+import setuptools
 
-setup(
-    name="bagpype",
-    version="",
-    description="",
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bagpype", # Replace with your own username
+    version="1.0",
     author="Ben Amor, Francesca Vianello, Florian Song",
-    author_email="",
-    url="",
-    packages=["numpy", "scipy", "networkx", "pandas", "matplotlib", "PyCifRw"],
+    author_email="florian.song@gmail.com",
+    description="Biomolecular, atomistic graph construction software in Python for proteins, etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FlorianSong/BagPype",
+    # project_urls={
+    #     "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+    install_requires=(
+        'numpy',
+        'scipy',
+        'networkx',
+        'pandas',
+        'PyCifRW',
+        'requests'
+    ),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    package_data={"bagpype": ["dependencies/*", "dependencies/mmcif/*"]},
+    python_requires=">=3.6",
 )
